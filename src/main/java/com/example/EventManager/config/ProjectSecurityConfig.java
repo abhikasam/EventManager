0bcfd1d127fbed0256975dd9f9918cba5d98990d
EventManager <abhinavkl1412@gmail.com>
2024-02-13 22:19:58 +0530

    integretedSecurity=true added dashboard with a sample tabs override login and logout added page level security -> with permitAll for now.

---
+++ src/main/java/com/example/EventManager/config/ProjectSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.EventManager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) ->
+                        requests.requestMatchers("/assets/**").permitAll()
+                                .requestMatchers("/dashboard").permitAll()
+                                .requestMatchers("/", "/home").permitAll()
+                                .requestMatchers("/static/**").permitAll()
+                                .requestMatchers("/login").permitAll()
+                                .requestMatchers("/logout").permitAll()
+                                .requestMatchers("/contact").permitAll()
+                                .requestMatchers("/about").permitAll())
+                .formLogin(loginConfigurer -> loginConfigurer.loginPage("/login")
+                        .defaultSuccessUrl("/dashboard").failureUrl("/login?error=true").permitAll())
+                .logout(logoutConfigurer -> logoutConfigurer.logoutSuccessUrl("/login?logout=true")
+                        .invalidateHttpSession(true).permitAll());
+        return http.build();
+    }
+}
